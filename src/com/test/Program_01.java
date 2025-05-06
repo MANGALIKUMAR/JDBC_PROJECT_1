072783a399ad9739be7ea408d8b894db2bc1ca2e
M Kumar <kumarbtecygr@gmail.com>
2025-05-06 09:33:42 +0530

    Update Program_01.java

--- src/com/test/Program_01.java
+++ src/com/test/Program_01.java
@@ -3,7 +3,11 @@ package com.test;
 public class Program_01 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Hello World!");
+		System.out.println("Enter Number : ");
+		int n = scan.nextInt();
+		System.out.println("square"+n*n);
 	}
 
 }
